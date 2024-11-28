cde1803923a9f33e522b482aaa0f3226bc48bf5d
Swarnil Singhai <31561157+imswarnil@users.noreply.github.com>
2024-11-28 14:19:08 -0800

    as

---
+++ assets/scripts/clipboard.js
@@ -0,0 +1,55 @@
+
+    document.addEventListener("DOMContentLoaded", function() {
+                const codeBlocks = document.querySelectorAll('pre code');
+                
+                codeBlocks.forEach(function(codeBlock) {
+                const parent = codeBlock.closest('.highlighter-rouge');
+                if (parent) {
+                    // Create language badge based on language class
+                    const language = parent.classList.contains('language-js') ? 'JavaScript' : 'Other';
+                    const languageBadge = document.createElement('span');
+                    languageBadge.classList.add('tag', 'is-primary', 'is-light');
+                    languageBadge.textContent = language;
+            
+                    // Create the copy button wrapper
+                    const copyButtonWrapper = document.createElement('div');
+                    copyButtonWrapper.classList.add('copy-btn-wrapper');
+            
+                    const copyButton = document.createElement('button');
+                    copyButton.classList.add('button', 'is-info', 'is-small', 'copy-btn');
+                    copyButton.textContent = 'Copy';
+                    
+                    // Event listener for copy button
+                    copyButton.addEventListener('click', function() {
+                    const range = document.createRange();
+                    range.selectNode(codeBlock);
+                    window.getSelection().removeAllRanges();
+                    window.getSelection().addRange(range);
+                    
+                    try {
+                        document.execCommand('copy');
+                        copyButton.innerHTML = `
+                        <span class="icon is-small">
+                            <i class="fas fa-check"></i>
+                        </span>
+                        <span>Copied!</span>
+                        `;
+                        setTimeout(() => {
+                        copyButton.innerHTML = `
+                            <span class="icon is-small">
+                            <i class="fas fa-copy"></i>
+                            </span>
+                            <span>Copy</span>
+                        `;
+                        }, 2000);
+                    } catch (err) {
+                        console.error('Copy failed', err);
+                    }
+                    });
+            
+                    copyButtonWrapper.appendChild(copyButton);
+                    parent.appendChild(languageBadge);
+                    parent.appendChild(copyButtonWrapper);
+                }
+                });
+    });
