eb4c647738881e90eff39f7176cd600340b16c22
Swarnil Singhai <31561157+imswarnil@users.noreply.github.com>
2024-12-02 04:46:20 -0800

    as

--- _plugins/lazy_loader.rb
+++ _plugins/lazy_loader.rb
@@ -18,40 +18,43 @@ module Jekyll
       item.output = lazy_load_ads(item.output)
     end
 
-    # Lazy load images by replacing src with data-src and adding a placeholder
+    # Lazy load images by replacing src with data-src
     def lazy_load_images(content)
       content.gsub(/<img([^>]*)src=["']([^"']*)["']([^>]*)>/i) do
-        <<~HTML
-          <img\1data-src="\2"\3 loading="lazy" class="lazy-image" alt="Lazy Loaded Image">
-        HTML
+        '<img\1data-src="\2"\3 class="lazy-image" loading="lazy" alt="Lazy Loaded Image">'
       end
     end
 
-    # Lazy load YouTube iframes by replacing them with a placeholder
+    # Lazy load YouTube iframes with styled placeholders
     def lazy_load_youtube(content)
       content.gsub(/<iframe([^>]*)src=["']https:\/\/www\.youtube\.com\/embed\/([^"']*)["']([^>]*)><\/iframe>/i) do
-        <<~HTML
-          <div class="youtube-placeholder" data-video-id="#{$2}">
-            <img class="youtube-thumbnail" data-src="https://img.youtube.com/vi/#{$2}/hqdefault.jpg" alt="YouTube Video Thumbnail">
-            <button class="youtube-play-button" aria-label="Play Video"></button>
+        %(
+          <div class="youtube-placeholder box has-text-centered" data-video-id="#{$2}">
+            <figure class="image is-16by9">
+              <img class="lazy-image youtube-thumbnail"
+                   data-src="https://img.youtube.com/vi/#{$2}/hqdefault.jpg"
+                   alt="YouTube Thumbnail">
+              <button class="button youtube-play-button is-primary">Play Video</button>
+            </figure>
           </div>
-        HTML
+        )
       end
     end
 
-    # Lazy load AdSense ads by replacing them with a placeholder
+    # Lazy load AdSense ads with placeholders
     def lazy_load_ads(content)
       content.gsub(/<ins class="adsbygoogle"([^>]*)><\/ins>/i) do
-        attributes = Hash[$1.scan(/\s*(\w+)="([^"]*)"/)]
-        <<~HTML
-          <div class="ad-placeholder" 
-               data-ad-client="#{attributes['data-ad-client']}" 
-               data-ad-slot="#{attributes['data-ad-slot']}" 
-               data-ad-style="#{attributes['style']}" 
-               #{attributes['data-ad-format'] ? "data-ad-format=\"#{attributes['data-ad-format']}\"" : ''}
-               #{attributes['data-ad-layout'] ? "data-ad-layout=\"#{attributes['data-ad-layout']}\"" : ''}>
+        %(
+          <div class="ad-placeholder box has-text-centered is-flex is-justify-content-center is-align-items-center"
+               data-ad-client="ca-pub-1291242080282540"
+               data-ad-slot="YOUR_AD_SLOT"
+               data-ad-style="display:block">
+            <span class="icon is-large">
+              <i class="fas fa-ad fa-2x"></i>
+            </span>
+            <p class="has-text-grey-light">Loading ad...</p>
           </div>
-        HTML
+        )
       end
     end
   end
