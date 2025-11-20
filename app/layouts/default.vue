1d44d0ab7f1001ce672360d34932ab8b7e86de92
Swarnil Singhai <31561157+imswarnil@users.noreply.github.com>
2025-11-20 06:50:45 +0000

    asas

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -2,13 +2,6 @@
   <div>
     <AppHeader />
     <template>
-  <Adsbygoogle
-    style="display:block"
-
-    :ad-full-width-responsive="true"
-    ad-client="ca-pub-1291242080282540"
-    ad-slot="7663977887"
-  />
 </template>
     <UMain>
       <slot />
--- app/pages/training/[...slug].vue
+++ app/pages/training/[...slug].vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 definePageMeta({
-  layout: 'docs'
+  layout: 'training'
 })
 
 const route = useRoute()
